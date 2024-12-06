2540d38dc17a2624a05e60c0ecec78b2b6f7c2f7
Apendable <martinpangrib41@gmail.com>
2024-12-06 09:50:59 +0700

    x

--- resources/views/app/doswal/home_doswal.blade.php
+++ resources/views/app/doswal/home_doswal.blade.php
@@ -193,176 +193,159 @@
     </style>
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <div class="container-fluid">
-        @php
-            $user = Auth::user();
-            $userRole = $user ? $user->role : null;
-        @endphp
+  <nav class="navbar navbar-expand-lg" style="background: linear-gradient(90deg, #0073e6, #003366);">
+  <div class="container-fluid">
+    @php
+      $user = Auth::user();
+      $userRole = $user ? $user->role : null;
+    @endphp
 
-        <a class="navbar-brand" href="
-            @if($userRole === 'Doswal') 
-                {{ route('home.doswal') }}
-            @elseif($userRole === 'Kaprodi') 
-                {{ route('home.kaprodi') }}
-            @elseif($userRole === 'Koordinator') 
-                {{ route('home.koordinator') }}
-            @elseif($userRole === 'Mahasiswa') 
-                {{ route('home.mahasiswa') }}
-            @else 
-                {{ route('home') }}
+    <a class="navbar-brand" href="
+      @if($userRole === 'Doswal') 
+          {{ route('home.doswal') }}
+      @elseif($userRole === 'Kaprodi') 
+          {{ route('home.kaprodi') }}
+      @elseif($userRole === 'Koordinator') 
+          {{ route('home.koordinator') }}
+      @else 
+          {{ route('home') }}
+      @endif
+    ">
+      <img
+        alt="Logo of the institution"
+        src="https://upload.wikimedia.org/wikipedia/commons/e/e2/Del_Institute_of_Technology_Logo.png"
+        style="height: 50px; margin-right: 10px"
+      />
+      <span style="font-size: 24px; color: white; font-weight: bold">PRATIKMA</span>
+    </a>
+
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarContent"
+      aria-controls="navbarContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" style="background-color: white;"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <!-- Beranda -->
+        <li class="nav-item">
+          <a class="nav-link
+            @if(in_array(Route::currentRouteName(), ['home', 'home.doswal', 'home.kaprodi', 'home.koordinator']))
+              active
             @endif
-        ">
-            <img
-                alt="Logo of the institution"
-                src="https://upload.wikimedia.org/wikipedia/commons/e/e2/Del_Institute_of_Technology_Logo.png"
-                style="height: 50px; margin-right: 10px"
-            />
-            <span style="font-size: 24px; color: white; font-weight: bold">PRATIKMA</span>
-        </a>
+          " href="
+            @if($userRole === 'Doswal') 
+              {{ route('home.doswal') }}
+            @elseif($userRole === 'Kaprodi') 
+              {{ route('home.kaprodi') }}
+            @elseif($userRole === 'Koordinator') 
+              {{ route('home.koordinator') }}
+            @else 
+              {{ route('home') }}
+            @endif
+          ">
+            Beranda
+          </a>
+        </li>
 
-        <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarContent"
-            aria-controls="navbarContent"
+        <!-- MBKM Dropdown -->
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdownMBKM"
+            role="button"
+            data-bs-toggle="dropdown"
             aria-expanded="false"
-            aria-label="Toggle navigation"
-        >
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <!-- Beranda Link -->
-                <li class="nav-item">
-                    <a class="nav-link
-                        @if(in_array(Route::currentRouteName(), ['home.doswal', 'home.kaprodi', 'home.koordinator', 'home.mahasiswa', 'home']))
-                            active
-                        @endif
-                    " href="
-                        @if($userRole === 'Doswal') 
-                            {{ route('home.doswal') }}
-                        @elseif($userRole === 'Kaprodi') 
-                            {{ route('home.kaprodi') }}
-                        @elseif($userRole === 'Koordinator') 
-                            {{ route('home.koordinator') }}
-                        @elseif($userRole === 'Mahasiswa') 
-                            {{ route('home.mahasiswa') }}
-                        @else 
-                            {{ route('home') }}
-                        @endif
-                    ">
-                        Beranda
-                    </a>
-                </li>
+          >
+            MBKM
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMBKM">
+            <li><a class="dropdown-item" href="#">Table Eligible MBKM</a></li>
+            <li><a class="dropdown-item" href="#">Table Pertimbangan MBKM</a></li>
+          </ul>
+        </li>
 
-                <!-- MBKM Dropdown -->
-                <li class="nav-item dropdown">
-                    <a
-                        class="nav-link dropdown-toggle"
-                        href="#"
-                        id="navbarDropdownMBKM"
-                        role="button"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                    >
-                        MBKM
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMBKM">
-                        <li>
-                            <a class="dropdown-item" href="{{ route('mbkm.informasi') }}">Table Eligible MBKM</a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="{{ route('kp.formkelayakan') }}">Table Pertimbangan MBKM</a>
-                        </li>
-                    </ul>
-                </li>
-
-                <!-- Kerja Praktik Dropdown -->
-                <li class="nav-item dropdown">
-                    <a
-                        class="nav-link dropdown-toggle"
-                        href="#"
-                        id="navbarDropdownKP"
-                        role="button"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                    >
-                        Kerja Praktik
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownKP">
-                        <li>
-                            <a class="dropdown-item" href="{{ route('kp.informasi') }}">Table Eligible KP</a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="
-                                @if($userRole === 'Doswal') 
-                                    {{ route('doswal.tabelinputkp') }}
-                                @elseif($userRole === 'Kaprodi') 
-                                    {{ route('kaprodi.tabelinputkp') }}
-                                @elseif($userRole === 'Koordinator') 
-                                    {{ route('koordinator.tabelinputkp') }}
-                                @elseif($userRole === 'Mahasiswa')
-                                    #
-                                @else
-                                    #
-                                @endif
-                            ">
-                                Table Pertimbangan KP
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-
-            <!-- Authentication Links -->
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login.form') }}">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">Register</a>
-                    </li>
+        <!-- Kerja Praktik Dropdown -->
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdownKP"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Kerja Praktik
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownKP">
+            <li>
+              <a class="dropdown-item" href="
+                @if($userRole === 'Doswal') 
+                  {{ route('doswal.tabelinputkp') }}
+                @elseif($userRole === 'Kaprodi') 
+                  {{ route('kaprodi.tabelinputkp') }}
+                @elseif($userRole === 'Koordinator') 
+                  {{ route('koordinator.tabelinputkp') }}
                 @else
-                    <!-- Notification Bell -->
-                    <li class="nav-item">
-                        <a href="#" onclick="showNotifications(event)" class="nav-link">
-                            <i class="fas fa-bell"></i>
-                            <span class="badge bg-danger">3</span>
-                        </a>
-                    </li>
+                  #
+                @endif
+              ">
+                Table Eligible KP
+              </a>
+            </li>
+            <li><a class="dropdown-item" href="#">Table Pertimbangan KP</a></li>
+          </ul>
+        </li>
 
-                    <!-- Profile Dropdown -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownUser" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            @if($user->profile_photo)
-                                <img src="{{ asset('storage/profile_photos/' . $user->profile_photo) }}" alt="Profile Photo" width="30" height="30" class="rounded-circle">
-                            @else
-                                <img src="{{ asset('images/default_profile.png') }}" alt="Default Profile Photo" width="30" height="30" class="rounded-circle">
-                            @endif
-                            {{ $user->name }}
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownUser">
-                            <li><a class="dropdown-item" href="{{ route('profile.show') }}">Profile</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li>
-                                <!-- Use POST for logout -->
-                                <a class="dropdown-item" href="#"
-                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </li>
-                        </ul>
-                    </li>
-                @endguest
+        @guest
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('login.form') }}">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">Register</a>
+          </li>
+        @else
+          <!-- Notification Bell -->
+          <li class="nav-item">
+            <a href="#" onclick="showNotifications(event)" class="nav-link">
+              <i class="fas fa-bell"></i>
+              <span class="badge bg-danger">3</span>
+            </a>
+          </li>
+
+          <!-- Profile Dropdown -->
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownUser" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              @if($user->profile_photo)
+                <img src="{{ asset('storage/profile_photos/' . $user->profile_photo) }}" alt="Profile Photo" width="30" height="30" class="rounded-circle">
+              @endif
+              {{ $user->name }}
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownUser">
+              <li><a class="dropdown-item" href="{{ route('profile.show') }}">Profile</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li>
+                <a class="dropdown-item" href="#"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                  Logout
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                  @csrf
+                </form>
+              </li>
             </ul>
-        </div>
+          </li>
+        @endguest
+      </ul>
     </div>
+  </div>
 </nav>
 
 
--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,19 @@ use App\Http\Controllers\KoordinatorKPController;
 Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
 Route::post('/login', [AuthController::class, 'login'])->name('post.login');
+
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register'])->name('post.register');
 
 Route::middleware(['auth'])->group(function () {
+    // Profile routes
     Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
     Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
 
+    // MBKM Routes
     Route::get('/home_mbkm', [MbkmController::class, 'informasi'])->name('mbkm.informasi');
+
+    // KP Routes for Mahasiswa
     Route::get('/formkelayakan_kp', [KpController::class, 'showFormKelayakanKP'])->name('kp.formkelayakan');
     Route::post('/formkelayakan_kp', [KpController::class, 'storeFormKelayakanKP'])->name('kp.formkelayakan.store');
     Route::get('/data_kelayakan', [KpController::class, 'showDataKelayakan'])->name('kp.data_kelayakan');
@@ -35,19 +40,19 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/home/kaprodi', [UsersController::class, 'kaprodiHome'])->name('home.kaprodi');
     Route::get('/home/koordinator', [UsersController::class, 'koordinatorHome'])->name('home.koordinator');
 
-    // Doswal KP routes
+    // Doswal KP routes (Table Eligible KP for Doswal)
     Route::get('/tabelinput_kp_doswal', [DoswalKPController::class, 'viewTableInputKPDoswal'])->name('doswal.tabelinputkp');
     Route::post('/tabelinput_kp_doswal/{id}/catatan', [DoswalKPController::class, 'updateCatatanDoswal'])->name('doswal.tabelinputkp.update_catatan');
 
-    // Kaprodi KP routes
+    // Kaprodi KP routes (Table Eligible KP for Kaprodi)
     Route::get('/tabelinput_kp_kaprodi', [KaprodiKPController::class, 'viewTableInputKPKaprodi'])->name('kaprodi.tabelinputkp');
     Route::post('/tabelinput_kp_kaprodi/{id}/catatan', [KaprodiKPController::class, 'updateCatatanKaprodi'])->name('kaprodi.tabelinputkp.update_catatan');
 
-    // Koordinator KP routes
+    // Koordinator KP routes (Table Eligible KP for Koordinator)
     Route::get('/tabelinput_kp_koordinator', [KoordinatorKPController::class, 'viewTableInputKPKoordinator'])->name('koordinator.tabelinputkp');
     Route::post('/tabelinput_kp_koordinator/{id}/catatan', [KoordinatorKPController::class, 'updateCatatanKoordinator'])->name('koordinator.tabelinputkp.update_catatan');
     Route::post('/tabelinput_kp_koordinator/{id}/status', [KoordinatorKPController::class, 'updateStatus'])->name('koordinator.tabelinputkp.update_status');
 
+    // Logout route
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
 });
